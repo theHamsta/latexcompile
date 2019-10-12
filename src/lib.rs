6e0ca5b70fdf44771bb12a47ee44926600a9d992
Stephan Seitz <stephan.seitz@fau.de>
2019-10-12 16:07:09 +0200

    Reexport tempdir

--- src/lib.rs
+++ src/lib.rs
@@ -57,7 +57,8 @@ use std::collections::HashMap;
 use std::fs;
 use std::path::{Path, PathBuf};
 use std::process::Command;
-use tempfile::{tempdir, TempDir};
+pub use tempfile::{tempdir, TempDir};
+
 
 pub struct LatexRunOptions {
     double_compilation: bool,
